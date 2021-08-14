f1449f96a93e5cf0d8eda84699e2c26c81527350
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-14 15:06:36 +0700

    register

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -2,7 +2,7 @@ import style from '../../styles/auth.module.css';
 import { InputAuth } from '../../components/base';
 import { Footer } from '../../components/module';
 import { useRouter } from 'next/router';
-const Login = () => {
+const SignUp = () => {
   const router = useRouter();
   return (
     <>
@@ -11,12 +11,12 @@ const Login = () => {
           <div className={style['container-content']}>
             <div className="w-full md:w-2/5 flex flex-col">
               <p className={style['text-banner']}>Le’ts Explore The World</p>
-              <p className={style['question-account']}>Don’t have account?</p>
+              <p className={style['question-account']}>Do you have account ?</p>
               <button
-                onClick={() => router.push('/auth/register')}
+                onClick={() => router.push('/auth/login')}
                 className="btn-secondary p-4 rounded-lg text-2xl font-bold mt-10"
               >
-                Sign Up
+                Login
               </button>
             </div>
             <div className="w-full md:w-1/5 block md:flex md:justify-center">
@@ -24,13 +24,13 @@ const Login = () => {
               <hr className="block md:hidden border-1 my-9" />
             </div>
             <div className="w-full md:w-2/5 flex flex-col flex-wrap">
-              <InputAuth styleContainer="md:mt-9" placeholder="Email" />
+              <InputAuth styleContainer="md:mt-9" placeholder="Name" />
+              <InputAuth placeholder="Email" />
               <InputAuth placeholder="Password" />
-              <p className="-mt-3 text-white underline font-normal">Forgot password?</p>
-              <button className="btn-primary p-4 rounded-lg text-2xl font-bold mt-10">Login</button>
+              <button className="btn-primary p-4 rounded-lg text-2xl font-bold">Sign Up</button>
               <button className="btn-white p-4 rounded-lg text-2xl font-bold mt-6 flex flex-row justify-center">
                 <img src="/assets/icon/google.png" alt="icon-google" />
-                <div className="ml-3">Login with Google</div>
+                <div className="ml-3">Sign Up with Google</div>
               </button>
             </div>
           </div>
@@ -41,4 +41,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
