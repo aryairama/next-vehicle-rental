45094523e19d8d46ba42a859bc1887ad6a6a1571
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-19 02:07:09 +0700

    read vahicle by type id

--- pages/type/index.jsx
+++ pages/type/index.jsx
@@ -1,12 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
 import { Navbar, Footer, CardTemplate, CardContainer, CardImgOverlay, CardTextOverlay } from '../../components/module';
 import { InputSearch } from '../../components/base';
-const index = () => {
+import { useRouter } from 'next/router';
+const Index = (props) => {
+  const router = useRouter()
   return (
     <>
       <Navbar auth={true} />
       <section id="search" className="mt-margin-navbar-1 container">
         <InputSearch placeholder="Search vehicle (ex, cars, cars name)" />
       </section>
+      {props?.types?.map((type, index) => (
+        <section key={index} id="populer-in-town" className="container mt-16">
+          <div className="flex flex-row flex-wrap w-full justify-between">
+            <p className="font-Playfair_Display font-bold text-2xl md:text-4xl w-1/2">{type.type_name}</p>
+            <div className="flex flex-row w-1/2 justify-end items-center gap-2 cursor-pointer" onClick={()=> router.push(`/type/${type.type_id}`)}>
+              <p className="font-Nunito text-orange text-base">View all</p>
+              <img className="h-3 w-2" src="/assets/icon/right-arrow-orange.png" alt="arrow" />
+            </div>
+          </div>
+          <CardTemplate>
+            {type.vehicles.map((vehicle, index) => (
+              <CardContainer styleCard="cursor-pointer" onClick={()=> router.push(`/vehicle/${vehicle.vehicle_id}`)} key={index}>
+                <CardImgOverlay src={`${process.env.NEXT_PUBLIC_API_URL}/${vehicle.vehicle_image}`} />
+                <CardTextOverlay>
+                  <p className="truncate font-semibold text-base">{vehicle.vehicles_name}</p>
+                  <p className="truncate text-grey-1">{vehicle.location_name}</p>
+                </CardTextOverlay>
+              </CardContainer>
+            ))}
+          </CardTemplate>
+        </section>
+      ))}
       <section id="populer-in-town" className="container mt-16">
         <div className="flex flex-row flex-wrap w-full justify-between">
           <p className="font-Playfair_Display font-bold text-2xl md:text-4xl w-1/2">Popular in town</p>
@@ -53,147 +78,23 @@ const index = () => {
           </CardContainer>
         </CardTemplate>
       </section>
-      <section id="cars" className="container mt-16">
-        <div className="flex flex-row flex-wrap w-full justify-between">
-          <p className="font-Playfair_Display font-bold text-2xl md:text-4xl w-1/2">Cars</p>
-          <div className="flex flex-row w-1/2 justify-end items-center gap-2">
-            <p className="font-Nunito text-orange text-base">View all</p>
-            <img className="h-3 w-2" src="/assets/icon/right-arrow-orange.png" alt="arrow" />
-          </div>
-        </div>
-        <CardTemplate>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/cars/1.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Van</p>
-              <p className="truncate text-grey-1">Yogyakarta</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/cars/2.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Lamborghini</p>
-              <p className="truncate text-grey-1">South Jakarta</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/cars/3.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Jeep</p>
-              <p className="truncate text-grey-1">Malang</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/cars/4.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">White Jeep</p>
-              <p className="truncate text-grey-1">Kalimantan</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/cars/5.jpg" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Bugatti Veyron </p>
-              <p className="truncate text-grey-1">Trenggalek</p>
-            </CardTextOverlay>
-          </CardContainer>
-        </CardTemplate>
-      </section>
-      <section id="motorbikes" className="container mt-16">
-        <div className="flex flex-row flex-wrap w-full justify-between">
-          <p className="font-Playfair_Display font-bold text-2xl md:text-4xl w-1/2">Motorbike</p>
-          <div className="flex flex-row w-1/2 justify-end items-center gap-2">
-            <p className="font-Nunito text-orange text-base">View all</p>
-            <img className="h-3 w-2" src="/assets/icon/right-arrow-orange.png" alt="arrow" />
-          </div>
-        </div>
-        <CardTemplate>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/motorbikes/1.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Vespa</p>
-              <p className="truncate text-grey-1">Yogyakarta</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/motorbikes/2.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Honda KLX</p>
-              <p className="truncate text-grey-1">Kalimantan</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/motorbikes/3.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Honda</p>
-              <p className="truncate text-grey-1">Malang</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/motorbikes/4.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Matic Bike</p>
-              <p className="truncate text-grey-1">Yogyakarta</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/motorbikes/5.jpg" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Kawasaki Ninja </p>
-              <p className="truncate text-grey-1">Trenggalek</p>
-            </CardTextOverlay>
-          </CardContainer>
-        </CardTemplate>
-      </section>
-      <section id="bikes" className="container my-16">
-        <div className="flex flex-row flex-wrap w-full justify-between">
-          <p className="font-Playfair_Display font-bold text-2xl md:text-4xl w-1/2">Bike</p>
-          <div className="flex flex-row w-1/2 justify-end items-center gap-2">
-            <p className="font-Nunito text-orange text-base">View all</p>
-            <img className="h-3 w-2" src="/assets/icon/right-arrow-orange.png" alt="arrow" />
-          </div>
-        </div>
-        <CardTemplate>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/bikes/1.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Fixie</p>
-              <p className="truncate text-grey-1">Yogyakarta</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/bikes/2.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Sport Bike</p>
-              <p className="truncate text-grey-1">Kalimantan</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/bikes/3.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Onthel </p>
-              <p className="truncate text-grey-1">Malang</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/bikes/4.png" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Fixie Gray </p>
-              <p className="truncate text-grey-1">Yogyakarta</p>
-            </CardTextOverlay>
-          </CardContainer>
-          <CardContainer>
-            <CardImgOverlay src="/assets/img/bikes/5.jpg" />
-            <CardTextOverlay>
-              <p className="truncate font-semibold text-base">Fixie Black </p>
-              <p className="truncate text-grey-1">Trenggalek</p>
-            </CardTextOverlay>
-          </CardContainer>
-        </CardTemplate>
-      </section>
       <Footer />
     </>
   );
 };
 
-export default index;
+export async function getServerSideProps(context) {
+  const types = await (await (await fetch(`${process.env.NEXT_PUBLIC_API_URL}/types`)).json()).data;
+  await Promise.all(
+    types.map(async (type,index) => {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/vehicles/type/${type.type_id}`);
+      const todo = await response.json();
+      types[index].vehicles = todo.data
+    })
+  );
+  return {
+    props: {types}, 
+  };
+}
+
+export default Index;
