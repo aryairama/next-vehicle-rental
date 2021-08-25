5258d087870db9dd45e75d5a090bc179a7d46640
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-25 17:39:45 +0700

    integerasi reservation

--- redux/action/reservationAction.js
+++ redux/action/reservationAction.js
@@ -1,4 +1,5 @@
 import { default as axios } from '../../configs/axiosConfig';
+import swal from 'sweetalert';
 
 export const addResrrvation = (data, history) => async (dispatch, getState) => {
   try {
@@ -6,6 +7,7 @@ export const addResrrvation = (data, history) => async (dispatch, getState) => {
       reservation: { reservation },
     } = getState();
     const reservationData = {
+      invoice_number: data.reservationPaymentCode,
       vehicle_id: reservation.vehicle_id,
       cost: reservation.reservation_total_price,
       start_date: reservation.reservation_start_date,
@@ -16,8 +18,10 @@ export const addResrrvation = (data, history) => async (dispatch, getState) => {
     };
     await axios.post('/reservations', reservationData);
     dispatch({ type: 'CLEAR_RESERVATION', payload: {} });
+    swal('Success', 'successfully added reservation data', 'success');
     history.push('/type');
   } catch (error) {
+    swal('Error', 'Failed added reservation data', 'error');
     console.log(error);
   }
 };
